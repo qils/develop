c177162b7499268c77756edd7240c049ebc5d127
admin <qls1991@163.com>
2018-03-01 20:39:51 +0800

    update

--- Unit3/hello.py
+++ Unit3/hello.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 
 @app.route('/test/<int:id_>/')
 def hello(id_):
-	print type(id_)
-	return 'Hello, World!'
+    print type(id_)
+
+    return 'Hello, World!'
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8888)
+    app.run(host='0.0.0.0', port=8888)
