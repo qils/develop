b03b060cd092db323491eedaa4504fb280734649
admin <qls1991@163.com>
2018-03-05 21:39:58 +0800

    update

--- Unit3/app_with_cli.py
+++ Unit3/app_with_cli.py
@@ -8,6 +8,6 @@ from flask import Flask
 app = Flask(__name__, template_folder='../templates/', static_folder='../static/')
 
 
-@app.cli.add_command()
+@app.cli.add_command(name='initdb')
 def initdb():
     click.echo('This is a test')
