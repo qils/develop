538fb34fa5dbaf84bc9eb3fad2b1d93ee90f18ac
admin <qls1991@163.com>
2018-03-05 21:35:39 +0800

    update

---
+++ Unit3/app_with_cli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+import click
+from flask import Flask
+
+
+app = Flask(__name__, template_folder='../templates/', static_folder='../static/')
+
+
+@app.cli.addK_command
+def initdb():
+    click.echo('This is a test')
