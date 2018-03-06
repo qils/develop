7b926f2ff94148c6519f92e6242903b34b7288ad
admin <qls1991@163.com>
2018-03-06 20:51:55 +0800

    update

---
+++ Unit3/app_with_macro.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='../templates/', static_folder='../static/')
+
+
+@app.route('/hello/')
+def hello():
+    return render_template('index.html', alist=[1, 2, 3])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888)
