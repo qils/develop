d5986af0dd924b7d76dc08853a8ef075d12d90e5
admin <qls1991@163.com>
2018-03-07 17:54:17 +0800

    update

---
+++ Unit3/app_with_exp_sql.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+import config
+from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, tuple_
+from sqlalchemy.sql import select, asc, and_
+
+
+eng = create_engine(config.DB_URI)
+meta = MetaData(eng)
+
+users = Table(
+    'users', meta,
+    'id', Column(Integer, primary_key=True, autoincrement=True),
+    'Name', Column(String(50), nullable=False)
+)
+
+
+if users.exists():
+    users.drop()
+users.create()
+
+
+def excute(s):
+    print '-' * 50
+    rs = con.execute(s)
+
+    for row in rs:
+        print row['id'], row['Name']
+
+
+with eng.connect() as con:
+    for username in ['mm1', 'mm2', 'mm3']:
+        user = users.insert().values(Name=username)
+        print str(user)
+        con.execute(user)
