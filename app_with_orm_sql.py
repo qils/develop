fc7e9aa5c8e07353b0b11fe03cba5da8364fbadc
admin <qls1991@163.com>
2018-03-07 20:00:50 +0800

    update

---
+++ app_with_orm_sql.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+import config
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy import and_, or_
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+eng = create_engine(config.DB_URI)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+
+
+Base.metadata.drop_all(bind=eng)
+Base.metadata.create_all(bind=eng)
+
+Session = sessionmaker(bind=eng)
+session = Session()
+
+
+session.add_all([User(name=username) for username in ('mm1', 'mm2', 'mm3')])
+
+
+def get_result(rs):
+    print '-' * 50
+    for row in rs:
+        print row.id, row.name
+
+
+rs = session.query(User).all()
+get_result(rs)
+
+
+rs = session.query(User).filter(and_(User.id > 2, User.id < 4))
+get_result(rs)
