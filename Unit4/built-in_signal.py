3a9933f927cf7c2f1b9806ceba5c4db45819788c
admin <qls1991@163.com>
2018-03-09 20:58:58 +0800

    update

--- Unit4/built-in_signal.py
+++ Unit4/built-in_signal.py
@@ -17,17 +17,45 @@ app.logger.addHandler(log_module(app.config['LOG_LEVEL']))
 
 
 def log_template_render(sender, template, context, **extra):
-    sender.logger.warn('template name: {}, content: {}'.format(template.name, context))
+    sender.logger.warn('Template name: {}, content: {}'.format(template.name, context))
 
 
 template_rendered.connect(log_template_render, app)
 
 
 def log_request_start(sender, **extra):
-    sender.logger.warn('request context start, remote address: {}'.format(request.remote_addr))
+    sender.logger.warn('Request context start, remote address: {}'.format(request.remote_addr))
 
 
 request_started.connect(log_request_start, app)
+
+
+def log_request_finished(sender, response, **extra):
+    sender.logger.warn('Request context is teardown')
+
+
+request_finished.connect(log_request_finished, app)
+
+
+def log_get_request_exception(sender, exception, **extra):
+    sender.logger.warn('Exception: {}'.format(exception))
+
+
+got_request_exception.connect(log_get_request_exception, app)
+
+
+def log_request_teardown(sender, **extra):
+    sender.logger.warn('Request context is tearing down')
+
+
+request_tearing_down.connect(log_request_teardown, app)
+
+
+def log_appcontext_teardown(sender, **extra):
+    sender.logger.warn('App context is tearing down')
+
+
+appcontext_tearing_down.connect(log_appcontext_teardown, app)
 
 
 @app.route('/')
