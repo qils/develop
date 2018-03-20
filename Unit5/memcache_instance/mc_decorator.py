b59c1e9e4ee1a56c29459a1803fe8cf5924055dc
admin <qls1991@163.com>
2018-03-20 20:58:20 +0800

    update

--- Unit5/memcache_instance/mc_decorator.py
+++ Unit5/memcache_instance/mc_decorator.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # --*-- coding: utf-8 --*--
 
+import inspect
+
 
 def cache(key_pattern, mc, expire, max_retry):
     print key_pattern, mc, expire, max_retry
 
     def wrapper(func):
+        arg_names, varargs, varkw, defaults = inspect.getargspec(func)
+        print arg_names, varargs, varkw, defaults
+
         def _(*args, **kwargs):
             pass
         return _
