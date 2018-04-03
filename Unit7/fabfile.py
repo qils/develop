a75f5ff882975bb71bdab7a677b421adbdd6226c
admin <qls1991@163.com>
2018-04-03 20:18:04 +0800

    update

---
+++ Unit7/fabfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+from fabric.api import run, local
+
+
+def hostname():
+    run('hostname')
+
+
+def ls(path='.'):
+    local('ls {}'.format(path))
