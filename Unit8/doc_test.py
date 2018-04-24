04f1054efb52722b5f3dbb89b7cfc9d69718a940
admin <qls1991@163.com>
2018-04-24 21:49:25 +0800

    update

---
+++ Unit8/doc_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+import doctest
+
+
+def import_object(name):
+    '''
+    imports object by name
+    >>> import os.path
+    >>> import_object('os.path') is os.path
+    True
+    >>> import_object('os.missing_module')
+    Traceback (most recent call last)
+        ...
+    ImportError: No module name missing_module
+    '''
+
+    parts = name.split('.')
+    obj = __import__('.'.join(parts[:-1]))
+
+    try:
+        return getattr(obj, parts[-1])
+    except AttributeError:
+        raise ImportError('No module name {}'.format(parts[-1]))
+
+
+if __name__ == '__main__':
+    doctest.testmod()
