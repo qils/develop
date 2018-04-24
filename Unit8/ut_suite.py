f935299b87736c5baa5b69ab28ff72a970ab180b
admin <qls1991@163.com>
2018-04-24 20:59:10 +0800

    update

---
+++ Unit8/ut_suite.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# --*-- coding: utf-8 --*--
+
+import unittest
+from collections import Counter
+
+
+class TestCounter(unittest.TestCase):
+    def startup(self):
+        self.c = Counter('abcdefabc')
+        print 'Start up ...'
+
+    def test_basic(self):
+        c = self.c
+        self.assertEqual(c, Counter(a=2, b=2, c=2, d=1, e=1, f=1))
+
+    def tearDown(self):
+        print 'Test teardown'
+
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(TestCounter)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
