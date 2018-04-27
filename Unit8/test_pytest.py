aed449514dc58326b2fb9ad65e8ed239ddb52884
admin <qls1991@163.com>
2018-04-27 21:22:52 +0800

    update

--- Unit8/test_pytest.py
+++ Unit8/test_pytest.py
@@ -14,3 +14,12 @@ def test_setup_math(setup_math):
     import time
     time.sleep(4)
     assert setup_math.pow(2, 3) == 8.0
+
+
+class TestClass(object):
+    def test_in(self):
+        assert 'h' in 'hello'
+
+    def test_two(self, setup_math):
+        asset setup_math.ceil(10) == 10
+
