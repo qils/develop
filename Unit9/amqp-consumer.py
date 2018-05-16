f1e0ac0d3a7afe6716dadba6806c84fe227403d1
admin <qls1991@163.com>
2018-05-16 21:38:09 +0800

    update

--- Unit9/amqp-consumer.py
+++ Unit9/amqp-consumer.py
@@ -9,6 +9,11 @@ def on_message(channel, method_frame, header_frame, body):
     print body
 
 
+def on_message1(channel, method_frame, header_frame, body):
+    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+    print body, 1
+
+
 parameters = pika.URLParameters('amqp://maomao:qls1991@localhost:5672/web_develop')
 connect = pika.BlockingConnection(parameters)
 channel = connect.channel()
@@ -18,6 +23,7 @@ channel.queue_declare('standrand1', durable=True, auto_delete=True)
 channel.queue_bind(queue='standrand1', exchange='web_develop', routing_key='xxx_routing_key')
 
 channel.basic_consume(on_message, 'standrand1')
+channel.basic_consume(on_message1, 'standrand1')
 
 try:
     channel.start_consuming()
